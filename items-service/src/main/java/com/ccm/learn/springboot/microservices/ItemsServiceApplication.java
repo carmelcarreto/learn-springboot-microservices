5d5a1b2e2bb95d188f79549bfa15d916573194e3
Catalina Carreto Melchor <isccatalinacarreto27@gmail.com>
2019-08-06 16:44:23 -0500

    chore: Create project items

---
+++ items-service/src/main/java/com/ccm/learn/springboot/microservices/ItemsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ccm.learn.springboot.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsServiceApplication.class, args);
+	}
+
+}
