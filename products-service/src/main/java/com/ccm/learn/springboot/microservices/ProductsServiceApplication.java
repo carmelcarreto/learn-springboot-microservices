ed8c32ecd7ccce7d6bce741ed32d2450d188d2b9
Catalina Carreto Melchor <catalina.carreto@tecnoaplicada.com>
2019-08-06 10:07:22 -0500

    chore: Update file application.properties

---
+++ products-service/src/main/java/com/ccm/learn/springboot/microservices/ProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ccm.learn.springboot.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+
+}
