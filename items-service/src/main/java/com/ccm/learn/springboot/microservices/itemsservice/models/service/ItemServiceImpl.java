6ae5023dfcd74ce304cff966781aa37e1ad683b2
Catalina Carreto Melchor <isccatalinacarreto27@gmail.com>
2019-08-08 13:52:47 -0500

    feat: Create restTemplate bean

--- items-service/src/main/java/com/ccm/learn/springboot/microservices/itemsservice/models/service/ItemServiceImpl.java
+++ items-service/src/main/java/com/ccm/learn/springboot/microservices/itemsservice/models/service/ItemServiceImpl.java
@@ -2,13 +2,18 @@ package com.ccm.learn.springboot.microservices.itemsservice.models.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.ccm.learn.springboot.microservices.itemsservice.models.Item;
 
 @Service
 public class ItemServiceImpl implements ItemService {
 
+	@Autowired
+	private RestTemplate clienteRest;
+	
 	@Override
 	public List<Item> findAll() {
 		
