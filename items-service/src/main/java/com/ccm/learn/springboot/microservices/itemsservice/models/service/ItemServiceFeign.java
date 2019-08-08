0b63310b85a1af6033ff0d8257a0171fdbcf882b
Catalina Carreto Melchor <isccatalinacarreto27@gmail.com>
2019-08-08 18:53:42 -0500

    feat: Implement client feign

---
+++ items-service/src/main/java/com/ccm/learn/springboot/microservices/itemsservice/models/service/ItemServiceFeign.java
@@ -0,0 +1,27 @@
+package com.ccm.learn.springboot.microservices.itemsservice.models.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import com.ccm.learn.springboot.microservices.itemsservice.clients.ProductRestClient;
+import com.ccm.learn.springboot.microservices.itemsservice.models.Item;
+
+@Service
+@Primary
+public class ItemServiceFeign implements ItemService {
+	
+	@Autowired
+	private ProductRestClient productClientFeign;
+
+	@Override
+	public List<Item> findAll() {
+		return productClientFeign.listar().stream().map(p -> new Item(p,1)).collect(Collectors.toList());
+	}
+
+	@Override
+	public Item findById(Long id, Integer cantidad) {
+		return new Item(productClientFeign.detalle(id),cantidad);
+	}
+}
