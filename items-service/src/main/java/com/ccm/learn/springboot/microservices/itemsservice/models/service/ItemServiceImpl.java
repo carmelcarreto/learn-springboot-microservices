469ba73d4b2f84a378b4d2e9bc1d56d2f984456e
Catalina Carreto Melchor <isccatalinacarreto27@gmail.com>
2019-08-08 16:21:33 -0500

    feat: Implement service component with HTTP restTemplate

--- items-service/src/main/java/com/ccm/learn/springboot/microservices/itemsservice/models/service/ItemServiceImpl.java
+++ items-service/src/main/java/com/ccm/learn/springboot/microservices/itemsservice/models/service/ItemServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ccm.learn.springboot.microservices.itemsservice.models.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.ccm.learn.springboot.microservices.itemsservice.models.Item;
+import com.ccm.learn.springboot.microservices.itemsservice.models.Product;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -16,14 +21,19 @@ public class ItemServiceImpl implements ItemService {
 	
 	@Override
 	public List<Item> findAll() {
-		
-		return null;
+		List<Product> productos 
+		= Arrays.asList(clienteRest.getForObject("http://localhost:8001/listar",
+				Product[].class));
+		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
 	@Override
 	public Item findById(Long id, Integer cantidad) {
-		
-		return null;
+		Map<String ,String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", id.toString());
+		Product product = clienteRest.getForObject("http://localhost:8001/ver/{id}",
+				Product.class, pathVariables);
+		return new Item(product, cantidad);
 	}
 
 }
