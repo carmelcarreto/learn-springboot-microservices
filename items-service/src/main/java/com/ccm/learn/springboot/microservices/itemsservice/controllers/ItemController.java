e5e6f17391220eab6f58e485f0863dc93caddc34
Catalina Carreto Melchor <isccatalinacarreto27@gmail.com>
2019-08-14 17:44:53 -0500

    refactor: Create eureka server and modify application.properties file

--- items-service/src/main/java/com/ccm/learn/springboot/microservices/itemsservice/controllers/ItemController.java
+++ items-service/src/main/java/com/ccm/learn/springboot/microservices/itemsservice/controllers/ItemController.java
@@ -15,7 +15,7 @@ import com.ccm.learn.springboot.microservices.itemsservice.models.service.ItemSe
 public class ItemController {
 
 	@Autowired
-	@Qualifier("serviceRestTemplate")
+	@Qualifier("serviceFeign")
 	private ItemService itemService;
 	
 	@GetMapping("/listar")
